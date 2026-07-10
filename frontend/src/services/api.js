4905b77213b3b489577358e6ec762b03e0b0ab07
Yogeshwaran.S <yogeshwarans565@gmail.com>
2026-07-10 11:19:29 +0530

    Change API base URL to production endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+ baseURL: "https://expense-tracker-6o0x.onrender.com/api",
 });
 
 // Attach token to every request if available
